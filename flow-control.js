dedda4080095a4f0d7fa8c2ce5cd33604d038980
Qize <louisliu1206@yahoo.com>
2020-06-06 20:49:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,7 +16,8 @@ return "You are a teenager!";
 } else if (age <=12) {
 return "You are a kid";
 } else {
-return "You are a grownup"}}
+return "You are a grownup";}
+}
 
 function ternaryTeenager(age) {
 if (age >= 13 && age <= 19) {
